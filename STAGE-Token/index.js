d592611cf22c7cce0016f62799ea3fee96a1a4b7
Manan Gupta <manan011299@gmail.com>
2024-04-18 11:32:35 +0530

    token

---
+++ STAGE-Token/index.js
@@ -0,0 +1,15 @@
+/*jshint node:true */
+"use strict";
+
+const fs = require('fs');
+const exchange = require("@adobe/aemcs-api-client-lib");
+
+const jsonfile = "aemcs-service-credentials.json";
+
+var config = JSON.parse(fs.readFileSync(jsonfile, 'utf8'));
+exchange(config).then(accessToken => {
+    // output the access token in json form including when it will expire.
+    console.log(JSON.stringify(accessToken,null,2));
+}).catch(e => {
+    console.log("Failed to exchange for access token ",e);
+});
